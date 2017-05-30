8f8776e50bd0897da0a7a8ec58c4abe1c3cac57b
Haruna Popoola <haruna.popoola@andela.com>
2017-05-30 16:46:10 +0100

    setup basic server configuration

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Set up the express app
+const app = express();
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`Started up at port port ${port}`);
+});
+
+module.exports = app;
