fec6759f19d81387ea3bf47760f1498743920114
Haruna Popoola <haruna.popoola@andela.com>
2017-06-01 16:06:42 +0100

    [feature 146302083] create /roles endpoint

---
+++ server/models/roles.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Roles = sequelize.define('Roles', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'The role title cannot be empty'
+        }
+      }
+    },
+  }, {
+    classMethods: {
+      associate(models) {
+        // associations can be defined here
+        Roles.hasMany(models.Users, {
+          foreignKey: 'roleId'
+        });
+      }
+    }
+  });
+  return Roles;
+};
