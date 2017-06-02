95d1e6b80b50297ea67771326662cc530d27bc5e
Haruna Popoola <haruna.popoola@andela.com>
2017-06-02 08:16:40 +0100

    [chore #146109443] setup materialize

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,21 @@
-const webpack = require('webpack');
+const path = require('path');
+// const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: [
-    'script!jquery/dist/jquery.min.js',
     'client/app.jsx'
   ],
-  externals: {
-    jquery: 'jQuery'
-  },
   plugins: [
-    new ExtractTextPlugin('public/style.css', {
+    new ExtractTextPlugin('css/style.css', {
       allChunks: true
     }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
+
   ],
   output: {
-    path: __dirname,
-    filename: './client/public/bundle.js'
+    path: path.join(__dirname, 'client/public/'),
+    filename: 'bundle.js'
   },
   resolve: {
     root: __dirname,
@@ -40,7 +34,7 @@ module.exports = {
       {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract('css!sass')
-      }
+      },
     ]
   },
   devtool: 'cheap-module-eval-source-map'
