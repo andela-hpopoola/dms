749ded7ed2343096afd0a887edfddaee0210cd81
Haruna Popoola <haruna.popoola@andela.com>
2017-06-28 17:49:54 +0100

    [Fix #147521167] fix houndci errors for lines that exceed 80 xters

--- client/components/layout/Sidebar.jsx
+++ client/components/layout/Sidebar.jsx
@@ -47,22 +47,40 @@ class Sidebar extends Component {
               <Link to="/dashboard"> <i className="fa fa-dashboard" /> &nbsp; Dashboard </Link>
             </li>
             <li className="collection-item">
-              <Link to="/new-document"> <i className="fa fa-file-text" /> &nbsp; New Documents </Link>
+              <Link to="/new-document">
+                <i className="fa fa-file-text" />
+                &nbsp; New Documents
+              </Link>
             </li>
             <li className="collection-item">
-              <Link to="/document/private"> <i className="fa fa-file" /> &nbsp; Personal Documents </Link>
+              <Link to="/document/private">
+                <i className="fa fa-file" />
+                &nbsp; Personal Documents
+              </Link>
             </li>
             <li className="collection-item">
-              <Link to="/document/public"> <i className="fa fa-file" /> &nbsp; Public Documents </Link>
+              <Link to="/document/public">
+                <i className="fa fa-file" />
+                &nbsp; Public Documents
+              </Link>
             </li>
             <li className="collection-item">
-              <Link to="/document/role"> <i className="fa fa-file" /> &nbsp; Role Documents </Link>
+              <Link to="/document/role">
+                <i className="fa fa-file" />
+                &nbsp; Role Documents
+              </Link>
             </li>
             <li className="collection-item">
-              <Link to="/edit-profile"> <i className="fa fa-user" /> &nbsp;  Edit Profile </Link>
+              <Link to="/edit-profile">
+                <i className="fa fa-user" />
+                &nbsp;  Edit Profile
+              </Link>
             </li>
             <li className="collection-item">
-              <Link to="/sign-out"> <i className="fa fa-sign-out" /> &nbsp; Log Out </Link>
+              <Link to="/sign-out">
+                <i className="fa fa-sign-out" />
+                &nbsp; Log Out
+              </Link>
             </li>
           </ul>
         </div>
